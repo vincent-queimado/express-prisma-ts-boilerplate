934599721620a9559a031316cc3a0c20b3a7f809
Vincent Queimado <Vincent.queimado@gmail.com>
2023-03-04 18:37:14 -0300

    Removed all files that are in the .gitignore

--- build/src/app/presenters/logs/_index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const listar_1 = __importDefault(require("./listar"));
-exports.default = {
-    listar: listar_1.default,
-};
--- build/src/app/services/commons/api_root.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => ({ success: true, data: '/info', error: null });
