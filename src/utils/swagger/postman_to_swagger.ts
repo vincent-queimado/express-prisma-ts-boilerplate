45473357c06a6d221bdd5a4d8c157411c40b7528
Vincent Queimado <Vincent.queimado@gmail.com>
2023-05-26 07:15:13 -0300

    fix: fix swagger doc api path
    fix swagger doc api path

--- src/utils/swagger/postman_to_swagger.ts
+++ src/utils/swagger/postman_to_swagger.ts
@@ -2,7 +2,6 @@ import postmanToOpenApi from 'postman-to-openapi';
 import yaml from 'yamljs';
 import path from 'path';
 
-import uri from '@utils/global_http_url/global_http_url';
 import apiUri from '@utils/global_api_path/global_api_path';
 import logger from '@utils/winston_file_logger/winston/logger';
 import { name, version, description, author, license } from '@packagejson';
