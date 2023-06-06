f2bd4b020100776f690065f31cf2e9302f3e2b4e
Vincent Queimado <Vincent.queimado@gmail.com>
2023-06-05 22:08:31 -0300

    feat: add zod schema
    add zod middleware and validade schema

--- src/routes/v1/user_auth_route.ts
+++ src/routes/v1/user_auth_route.ts
@@ -1,16 +1,19 @@
 import { Router } from 'express';
 
-import CtrlUserAuth from '@controllers/client/users_auth_controller';
 import Auth from '@middlewares/jwt_auth/auth';
+import CtrlUserAuth from '@controllers/client/users_auth_controller';
+
+import { register, login } from '@schemas/auth_schema';
+import { validateSchema } from '@middlewares/validate_schema/validade_schema';
 
 const router = Router();
 
 // User Register
-router.post('/register', CtrlUserAuth.register);
+router.post('/register', validateSchema(register), CtrlUserAuth.register);
 router.get('/register/confirmation', CtrlUserAuth.registerConfirm);
 
 // User Login/Logout
-router.post('/login', CtrlUserAuth.login);
+router.post('/login', validateSchema(login), CtrlUserAuth.login);
 router.get('/logout', Auth.jwtUsers, CtrlUserAuth.logout);
 
 // User Forgot Password
